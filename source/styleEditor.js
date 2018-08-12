a76426b9b58884f464d04237f1eece43507d8cbe
JamisonOrdway <jamisonordway@gmail.com>
2018-08-12 16:57:50 -0600

    implements style editor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
   // your code goes here.
+  function changeStyle(selector, property, value) {
+    $(selector).css(property, value);
+  }
+
+  let style_editor = $('#style_editor');
+
+  form.submit(function(event) {
+    let selector = $("input[name='selector']").val();
+    let property = $("input[name='property']").val();
+    let value = $("input[name='value']").val();
+
+    changeStyle(selector, property, value);
+    event.preventDefault();
+  })
 });
